814ab33ceea0a651a8a3ba5f5bf1165d8efee6d1
Yuriy Nemtsov <nemtsov@gmail.com>
2017-03-27 11:06:46 -0400

    httpclient using plugin client

--- node/server.js
+++ node/server.js
@@ -1,10 +1,26 @@
 const http = require('http');
 const fs = require('fs');
 
-const PORT_OR_SOCKET = '/var/run/docker/comm.sock';
+const PORT_OR_SOCKET = process.env.PORT || '/var/run/docker/comm.sock';
 const isSocket = isNaN(Number(PORT_OR_SOCKET));
 
+function getData(req) {
+  return new Promise((resolve, reject) => {
+    const buffers = [];
+    req.on('data', data => buffers.push(data));
+    req.on('end', () => resolve(Buffer.concat(buffers)));
+  });
+}
+
 const server = http.createServer((req, res) => {
+
+  console.log('NODE-METH:', req.method);
+  console.log('NODE-HEADERS:', req.headers);
+
+  getData(req).then((data) => {
+    console.log('DATA: ', data.toString());
+  });
+
   res.end('Hello! From Node.js');
 });
 
--- php/html/index.php
+++ php/html/index.php
@@ -1,21 +1,46 @@
 <?php
 
-require(realpath(__DIR__ . '/../vendor/autoload.php'));
+require( realpath( __DIR__ . '/../vendor/autoload.php' ) );
 
+use Http\Client\Common\Plugin\ContentLengthPlugin;
+use Http\Client\Common\Plugin\StopwatchPlugin;
+use Http\Client\Common\PluginClient;
 use Http\Client\Socket\Client;
-use GuzzleHttp\Psr7\Request;
+use Http\Discovery\MessageFactoryDiscovery;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 $PORT_OR_SOCKET = '/var/run/docker/comm.sock';
 
-$client = new Client(null, [
-  'remote_socket' => "unix://{$PORT_OR_SOCKET}"
-]);
-
 $start_time = microtime( true );
-$request = new Request('GET', '/');
-$response = $client->sendRequest($request);
-$body = $response->getBody();
+//-----------------------
+
+$stopwatch = new Stopwatch();
+
+$message_factory = MessageFactoryDiscovery::find();
+
+$client = new PluginClient(
+    new Client( $message_factory, [
+        'remote_socket' => "unix://{$PORT_OR_SOCKET}"
+    ] ),
+    [
+        new ContentLengthPlugin(),
+        new StopwatchPlugin( $stopwatch )
+    ]
+);
+
+$request  = $message_factory->createRequest( 'POST', '/', [], 'HELLO' );
+$response = $client->sendRequest( $request );
+$body     = $response->getBody();
+
+
+//-----------------------
 $time_diff_in_ms = (microtime( true ) - $start_time) * 1000;
 
 echo $body . "\n";
 echo (int) $time_diff_in_ms . 'ms';
+
+foreach ($stopwatch->getSections() as $section) {
+    foreach ($section->getEvents() as $name => $event) {
+        echo sprintf('Request %s took %s ms and used %s bytes of memory', $name, $event->getDuration(), $event->getMemory());
+    }
+}
